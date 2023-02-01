c9a2d8e6c1180c26c5e8a9e422a6ea44d5b1b0e1
Sun Keun Choi <sunkeun.choi@gmail.com>
2023-02-01 18:19:38 +0900

    [feature/videos] RefreshIndicator

--- lib/features/navigation/main_navigation_screen.dart
+++ lib/features/navigation/main_navigation_screen.dart
@@ -31,6 +31,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: _selectedIndex == 0 ? Colors.black : Colors.white,
       body: Stack(
         children: [
           Offstage(
--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -98,124 +98,138 @@ class _VideoPostState extends State<VideoPost>
     });
   }
 
+  Future<void> _onRefresh() {
+    return Future.delayed(
+      const Duration(
+        seconds: 5,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return VisibilityDetector(
-      key: Key("${widget.index}"),
-      onVisibilityChanged: _onVisibilityChanged,
-      child: Stack(
-        children: [
-          Positioned.fill(
-            child: _videoPlayerController.value.isInitialized
-                ? VideoPlayer(_videoPlayerController)
-                : Container(color: Colors.black),
-          ),
-          Positioned.fill(
-            child: GestureDetector(
-              onTap: _onTogglePause,
+    return RefreshIndicator(
+      onRefresh: _onRefresh,
+      displacement: 50,
+      edgeOffset: 20,
+      color: Theme.of(context).primaryColor,
+      child: VisibilityDetector(
+        key: Key("${widget.index}"),
+        onVisibilityChanged: _onVisibilityChanged,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: _videoPlayerController.value.isInitialized
+                  ? VideoPlayer(_videoPlayerController)
+                  : Container(color: Colors.black),
             ),
-          ),
-          Positioned.fill(
-            child: IgnorePointer(
-              child: Center(
-                child: AnimatedBuilder(
-                  animation: _animationController,
-                  builder: (context, child) {
-                    return Transform.scale(
-                      scale: _animationController.value,
-                      child: child,
-                    );
-                  },
-                  child: AnimatedOpacity(
-                    opacity: _isPaused ? 1 : 0,
-                    duration: _animationDuration,
-                    child: const FaIcon(
-                      FontAwesomeIcons.play,
-                      color: Colors.white,
-                      size: Sizes.size64,
+            Positioned.fill(
+              child: GestureDetector(
+                onTap: _onTogglePause,
+              ),
+            ),
+            Positioned.fill(
+              child: IgnorePointer(
+                child: Center(
+                  child: AnimatedBuilder(
+                    animation: _animationController,
+                    builder: (context, child) {
+                      return Transform.scale(
+                        scale: _animationController.value,
+                        child: child,
+                      );
+                    },
+                    child: AnimatedOpacity(
+                      opacity: _isPaused ? 1 : 0,
+                      duration: _animationDuration,
+                      child: const FaIcon(
+                        FontAwesomeIcons.play,
+                        color: Colors.white,
+                        size: Sizes.size64,
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: Sizes.size32,
-            left: Sizes.size32,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "@니꼬",
-                  style: TextStyle(
-                    fontSize: Sizes.size20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Gaps.v10,
-                Row(
-                  children: [
-                    Text(
-                      widget._hasMore && !_seeMore
-                          ? "${widget.description.substring(0, 15)}..."
-                          : widget.description,
-                      style: const TextStyle(
-                        fontSize: Sizes.size16,
-                        color: Colors.white,
-                      ),
+            Positioned(
+              bottom: Sizes.size32,
+              left: Sizes.size32,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "@니꼬",
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
                     ),
-                    if (widget._hasMore && !_seeMore) Gaps.h05,
-                    if (widget._hasMore && !_seeMore)
-                      GestureDetector(
-                        onTap: _onTapSeeMore,
-                        child: const Text(
-                          "See More",
-                          style: TextStyle(
-                            fontSize: Sizes.size16,
-                            color: Colors.white,
-                          ),
-                        ),
-                      )
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: Sizes.size12,
-            right: Sizes.size12,
-            child: Column(
-              children: const [
-                CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.black,
-                  foregroundColor: Colors.white,
-                  foregroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/639005?v=4"),
-                  child: Text(
-                    "니꼬",
                   ),
-                ),
-                Gaps.v20,
-                VideoButton(
-                  icon: FontAwesomeIcons.solidHeart,
-                  text: "2.9M",
-                ),
-                Gaps.v20,
-                VideoButton(
-                  icon: FontAwesomeIcons.solidComment,
-                  text: "33K",
-                ),
-                Gaps.v20,
-                VideoButton(
-                  icon: FontAwesomeIcons.share,
-                  text: "Share",
-                )
-              ],
+                  Gaps.v10,
+                  Row(
+                    children: [
+                      Text(
+                        widget._hasMore && !_seeMore
+                            ? "${widget.description.substring(0, 15)}..."
+                            : widget.description,
+                        style: const TextStyle(
+                          fontSize: Sizes.size16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      if (widget._hasMore && !_seeMore) Gaps.h05,
+                      if (widget._hasMore && !_seeMore)
+                        GestureDetector(
+                          onTap: _onTapSeeMore,
+                          child: const Text(
+                            "See More",
+                            style: TextStyle(
+                              fontSize: Sizes.size16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Positioned(
+              bottom: Sizes.size12,
+              right: Sizes.size12,
+              child: Column(
+                children: const [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    foregroundImage: NetworkImage(
+                        "https://avatars.githubusercontent.com/u/639005?v=4"),
+                    child: Text(
+                      "니꼬",
+                    ),
+                  ),
+                  Gaps.v20,
+                  VideoButton(
+                    icon: FontAwesomeIcons.solidHeart,
+                    text: "2.9M",
+                  ),
+                  Gaps.v20,
+                  VideoButton(
+                    icon: FontAwesomeIcons.solidComment,
+                    text: "33K",
+                  ),
+                  Gaps.v20,
+                  VideoButton(
+                    icon: FontAwesomeIcons.share,
+                    text: "Share",
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
