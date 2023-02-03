21a4e38eabbee98aec787e6134a4533a10d75fb5
Sun Keun Choi <sunkeun.choi@gmail.com>
2023-02-03 17:31:58 +0900

    [feature/discover] GridView SliverGridDelegateWithFixedCrossAxisCount

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -41,7 +41,27 @@ class DiscoverScreen extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            for (var tab in tabs)
+            GridView.builder(
+              padding: const EdgeInsets.all(
+                Sizes.size06,
+              ),
+              itemCount: 20,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: Sizes.size10,
+                mainAxisSpacing: Sizes.size10,
+                childAspectRatio: 9 / 16,
+              ),
+              itemBuilder: (context, index) => Container(
+                color: Colors.amber,
+                child: Center(
+                  child: Text(
+                    index.toString(),
+                  ),
+                ),
+              ),
+            ),
+            for (var tab in tabs.skip(1))
               Center(
                 child: Text(
                   tab,
