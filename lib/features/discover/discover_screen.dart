6d732f209d949823936b5044841b169636e3b551
Sun Keun Choi <sunkeun.choi@gmail.com>
2023-02-03 17:22:59 +0900

    [feature/discover] TabBar TabBarView

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ticktoc/constants/sizes.dart';
+
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "LIVE",
+  "Shopping",
+  "Brands",
+];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Discover',
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: const Text(
+            'Discover',
+          ),
+          bottom: TabBar(
+            unselectedLabelColor: Colors.grey.shade500,
+            labelColor: Colors.black,
+            indicatorColor: Colors.black,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: Sizes.size16,
+            ),
+            isScrollable: true,
+            splashFactory: NoSplash.splashFactory,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size16,
+            ),
+            tabs: [for (var tab in tabs) Tab(text: tab)],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            for (var tab in tabs)
+              Center(
+                child: Text(
+                  tab,
+                  style: const TextStyle(
+                    fontSize: Sizes.size28,
+                  ),
+                ),
+              )
+          ],
         ),
       ),
     );
