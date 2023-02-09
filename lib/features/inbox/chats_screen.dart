443956454ca501bad718a42493abd5b4dcad19bb
sunkeunchoi <sunkeun.choi@gmail.com>
2023-02-09 17:08:16 +0900

    [feature/chat] AnimatedList removeItem

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ticktoc/assets/image.dart';
 import 'package:flutter_ticktoc/constants/sizes.dart';
+import 'package:flutter_ticktoc/features/inbox/chat_detail_screen.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ChatsScreen extends StatefulWidget {
@@ -12,16 +13,70 @@ class ChatsScreen extends StatefulWidget {
 
 class _ChatsScreenState extends State<ChatsScreen> {
   final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+  final Duration _duration = const Duration(
+    milliseconds: 300,
+  );
   void _addItem() {
     if (_key.currentState == null) return;
     // _key.currentState?.insertItem(0);
     _key.currentState?.insertItem(
       _items.length,
-      duration: const Duration(
-        milliseconds: 500,
-      ),
+      duration: _duration,
     );
     _items.add(_items.length);
+  }
+
+  void _deleteItem(int index) {
+    _key.currentState?.removeItem(
+      index,
+      (context, animation) => SizeTransition(
+        sizeFactor: animation,
+        child: Container(
+          color: Colors.red,
+          child: _makeTile(index),
+        ),
+      ),
+      duration: _duration,
+    );
+    _items.removeAt(index);
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      leading: const CircleAvatar(
+        radius: Sizes.size28,
+        foregroundImage: NetworkImage(
+          ExampleImage.profile1,
+        ),
+        child: Text("니꼬"),
+      ),
+      title: Text(
+        "Lynn $index",
+        style: const TextStyle(
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      subtitle: const Text(
+        "Don't forget to make video",
+      ),
+      trailing: Text(
+        "2:16 PM",
+        style: TextStyle(
+          color: Colors.grey.shade500,
+          fontSize: Sizes.size12,
+        ),
+      ),
+    );
   }
 
   final List<int> _items = [];
@@ -57,31 +112,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
               opacity: animation,
               child: SizeTransition(
                 sizeFactor: animation,
-                child: ListTile(
-                  leading: const CircleAvatar(
-                    radius: Sizes.size28,
-                    foregroundImage: NetworkImage(
-                      ExampleImage.profile1,
-                    ),
-                    child: Text("니꼬"),
-                  ),
-                  title: Text(
-                    "Lynn $index",
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  subtitle: const Text(
-                    "Don't forget to make video",
-                  ),
-                  trailing: Text(
-                    "2:16 PM",
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
-                      fontSize: Sizes.size12,
-                    ),
-                  ),
-                ),
+                child: _makeTile(index),
               ),
             ),
           );
