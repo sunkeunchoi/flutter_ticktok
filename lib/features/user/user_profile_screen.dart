6ceffa77aec37dcf5427b96ae8317310cbfac0ca
sunkeunchoi <sunkeun.choi@gmail.com>
2023-02-10 12:48:34 +0900

    [feature/profile] SliverAppBar FlexibleSpaceBar

--- lib/features/user/user_profile_screen.dart
+++ lib/features/user/user_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ticktoc/assets/image.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -10,6 +11,21 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: Colors.teal,
+          collapsedHeight: 100,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image.asset(
+              ExampleImage.example1,
+              fit: BoxFit.cover,
+            ),
+            title: const Text("Hello"),
+          ),
+        ),
+      ],
+    );
   }
 }
