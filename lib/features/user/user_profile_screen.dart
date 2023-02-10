f46829aeea30a28a091498c583e9e40d44b6fa90
sunkeunchoi <sunkeun.choi@gmail.com>
2023-02-10 14:17:51 +0900

    [feature/profile] TabBar

--- lib/features/user/user_profile_screen.dart
+++ lib/features/user/user_profile_screen.dart
@@ -14,74 +14,164 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: const Text("니꼬"),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const FaIcon(
-                FontAwesomeIcons.gear,
-                size: Sizes.size20,
+    return DefaultTabController(
+      length: 2,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: const Text("니꼬"),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const FaIcon(
+                  FontAwesomeIcons.gear,
+                  size: Sizes.size20,
+                ),
               ),
-            ),
-          ],
-        ),
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              const CircleAvatar(
-                radius: Sizes.size52,
-                foregroundColor: Colors.teal,
-                foregroundImage: NetworkImage(ExampleImage.profile1),
-              ),
-              Gaps.v20,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "@니꼬",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: Sizes.size20,
-                    ),
-                  ),
-                  Gaps.h05,
-                  FaIcon(
-                    FontAwesomeIcons.solidCircleCheck,
-                    size: Sizes.size20,
-                    color: Colors.blue.shade500,
-                  ),
-                ],
-              ),
-              Gaps.v20,
-              SizedBox(
-                height: Sizes.size52,
-                child: Row(
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const CircleAvatar(
+                  radius: Sizes.size52,
+                  foregroundColor: Colors.teal,
+                  foregroundImage: NetworkImage(ExampleImage.profile1),
+                ),
+                Gaps.v20,
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    UserCount(
-                      counts: "37",
-                      title: "Following",
+                  children: [
+                    const Text(
+                      "@니꼬",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: Sizes.size20,
+                      ),
                     ),
-                    Divider(),
-                    UserCount(
-                      counts: "10M",
-                      title: "Followers",
-                    ),
-                    Divider(),
-                    UserCount(
-                      counts: "194.3M",
-                      title: "Likes",
+                    Gaps.h05,
+                    FaIcon(
+                      FontAwesomeIcons.solidCircleCheck,
+                      size: Sizes.size20,
+                      color: Colors.blue.shade500,
                     ),
                   ],
                 ),
-              )
-            ],
-          ),
-        )
-      ],
+                Gaps.v20,
+                SizedBox(
+                  height: Sizes.size52,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      UserCount(
+                        counts: "37",
+                        title: "Following",
+                      ),
+                      Divider(),
+                      UserCount(
+                        counts: "10M",
+                        title: "Followers",
+                      ),
+                      Divider(),
+                      UserCount(
+                        counts: "194.3M",
+                        title: "Likes",
+                      ),
+                    ],
+                  ),
+                ),
+                Gaps.v14,
+                FractionallySizedBox(
+                  widthFactor: 0.33,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: Sizes.size14,
+                      horizontal: Sizes.size10,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        Sizes.size05,
+                      ),
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    child: const Text(
+                      "Follow",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Gaps.v14,
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: Sizes.size32,
+                  ),
+                  child: Text(
+                    "All highlights and where to watch live matches on FIFA + I wonder how it loook,",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Gaps.v14,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    FaIcon(
+                      FontAwesomeIcons.link,
+                      size: Sizes.size14,
+                    ),
+                    Gaps.h04,
+                    Text(
+                      "https://nomadcoders.co",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Gaps.v20,
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.symmetric(
+                      horizontal: BorderSide(
+                        color: Colors.grey.shade300,
+                        width: Sizes.size01,
+                      ),
+                    ),
+                  ),
+                  child: const TabBar(
+                    labelColor: Colors.black,
+                    indicatorColor: Colors.black,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    tabs: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: Sizes.size18,
+                          vertical: Sizes.size12,
+                        ),
+                        child: FaIcon(
+                          FontAwesomeIcons.table,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: Sizes.size18,
+                          vertical: Sizes.size12,
+                        ),
+                        child: FaIcon(
+                          FontAwesomeIcons.heart,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
