46c4c40d561b209e2668b9826eea6fad1e4dea92
sunkeunchoi <sunkeun.choi@gmail.com>
2023-02-10 15:15:37 +0900

    [feature/profile] NestedScrollView SliverPersistentHeader TabBarView

--- lib/assets/image.dart
+++ lib/assets/image.dart
@@ -1,5 +1,7 @@
 class ExampleImage {
   static const String example1 = "assets/images/man-g1e71e85cf_1920.jpg";
+  static const String network1 =
+      "https://images.unsplash.com/photo-1525340581945-d5e2b09641c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80";
   static const String profile1 =
       "https://avatars.githubusercontent.com/u/639005?v=4";
 }
