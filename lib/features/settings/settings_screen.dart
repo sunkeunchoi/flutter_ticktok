9de9d5acbabbb9570400070228190ce696c99c9e
sunkeunchoi <sunkeun.choi@gmail.com>
2023-02-10 19:52:32 +0900

    [feature/settings] CupertinoActivityIndicator CircularProgressIndicator CloseButton

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -10,42 +11,12 @@ class SettingsScreen extends StatelessWidget {
           'Settings',
         ),
       ),
-      body: ListWheelScrollView(
-        // offAxisFraction: 2,
-        diameterRatio: 10,
-        useMagnifier: true,
-        magnification: 2,
-        itemExtent: 100,
-        children: [
-          for (var x in [
-            1,
-            2,
-            3,
-            4,
-            5,
-            5,
-            6,
-            7,
-            7,
-            8,
-            8,
-            9,
-            9,
-          ])
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Container(
-                color: Colors.teal,
-                alignment: Alignment.center,
-                child: const Text(
-                  "Pick me",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
+      body: Column(
+        children: const [
+          CupertinoActivityIndicator(),
+          CircularProgressIndicator(),
+          CloseButton(),
+          CircularProgressIndicator.adaptive()
         ],
       ),
     );
