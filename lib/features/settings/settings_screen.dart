0948e7fae5ce2ebdda49feccf4ce5a7d43456dc0
sunkeunchoi <sunkeun.choi@gmail.com>
2023-02-10 22:29:50 +0900

    [feature/settings] SwitchListTile CheckboxListTile

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifiactions = false;
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifiactions = newValue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,38 +28,36 @@ class SettingsScreen extends StatelessWidget {
       body: ListView(
         children: [
           const AboutListTile(),
-          ListTile(
-            onTap: () async {
-              final date = await showDatePicker(
-                context: context,
-                initialDate: DateTime.now(),
-                firstDate: DateTime(1980),
-                lastDate: DateTime(2030),
-              );
-              print(date);
-              final time = await showTimePicker(
-                context: context,
-                initialTime: TimeOfDay.now(),
-              );
-              print(time);
-              final booking = await showDateRangePicker(
-                context: context,
-                firstDate: DateTime(1980),
-                lastDate: DateTime(2030),
-                builder: (context, child) => Theme(
-                  data: ThemeData(
-                    appBarTheme: const AppBarTheme(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.black,
-                    ),
-                  ),
-                  child: child!,
-                ),
-              );
-              print(booking);
-            },
+          SwitchListTile.adaptive(
+            value: _notifiactions,
+            onChanged: _onNotificationsChanged,
+          ),
+          SwitchListTile(
+            value: _notifiactions,
+            onChanged: _onNotificationsChanged,
+          ),
+          Switch.adaptive(
+            value: _notifiactions,
+            onChanged: _onNotificationsChanged,
+          ),
+          CupertinoSwitch(
+            value: _notifiactions,
+            onChanged: _onNotificationsChanged,
+          ),
+          Switch(
+            value: _notifiactions,
+            onChanged: _onNotificationsChanged,
+          ),
+          Checkbox(
+            value: _notifiactions,
+            onChanged: _onNotificationsChanged,
+          ),
+          CheckboxListTile(
+            value: _notifiactions,
+            onChanged: _onNotificationsChanged,
+            activeColor: Colors.black,
             title: const Text(
-              "What is your birthday?",
+              "Enable notifications",
             ),
           ),
         ],
