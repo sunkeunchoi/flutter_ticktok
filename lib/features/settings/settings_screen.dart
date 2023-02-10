21491fe82e8e18473113616b614d3c8bd3aa9ef0
sunkeunchoi <sunkeun.choi@gmail.com>
2023-02-10 19:58:22 +0900

    [feature/settings] AboutListTile showAboutDialog

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -11,12 +10,25 @@ class SettingsScreen extends StatelessWidget {
           'Settings',
         ),
       ),
-      body: Column(
-        children: const [
-          CupertinoActivityIndicator(),
-          CircularProgressIndicator(),
-          CloseButton(),
-          CircularProgressIndicator.adaptive()
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () => showAboutDialog(
+                context: context,
+                applicationVersion: "1.0",
+                applicationLegalese:
+                    "All rights reserved. Please dont copy me."),
+            title: const Text(
+              "About",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            subtitle: const Text(
+              "About this app.....",
+            ),
+          ),
+          const AboutListTile(),
         ],
       ),
     );
