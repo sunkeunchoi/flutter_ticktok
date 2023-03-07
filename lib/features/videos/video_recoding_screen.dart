7ad2435a10b4f8dca993906aaa075e2c576e0eed
sunkeunchoi <sunkeun.choi@gmail.com>
2023-03-07 21:10:25 +0900

    19.5 onTapDown,onTapUp,onLongPressedEnd+Animations

--- lib/features/videos/video_recoding_screen.dart
+++ lib/features/videos/video_recoding_screen.dart
@@ -14,14 +14,28 @@ class VideoRecodingScreen extends StatefulWidget {
   State<VideoRecodingScreen> createState() => _VideoRecodingScreenState();
 }
 
-class _VideoRecodingScreenState extends State<VideoRecodingScreen> {
+class _VideoRecodingScreenState extends State<VideoRecodingScreen>
+    with TickerProviderStateMixin {
   late CameraController _cameraController;
   bool _hasPermission = false;
   bool _isSelfieMode = true;
   late FlashMode _flashMode;
+  late final AnimationController _progressAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 10),
+    lowerBound: 0.0,
+    upperBound: 1.0,
+  );
+  late final AnimationController _buttonAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 100),
+  );
+  late final Animation<double> _buttonAnimation =
+      Tween<double>(begin: 1.0, end: 1.2).animate(_buttonAnimationController);
   Future<void> initCamera() async {
     final cameras = await availableCameras();
-    print(cameras);
     if (cameras.isEmpty || cameras.length < 2) return;
     _cameraController = CameraController(
       _isSelfieMode ? cameras[1] : cameras[0],
@@ -63,17 +77,70 @@ class _VideoRecodingScreenState extends State<VideoRecodingScreen> {
     super.initState();
     initPermissions();
     initCamera();
+    _progressAnimationController.addListener(() {
+      setState(() {});
+    });
+    _progressAnimationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _stopRecording();
+      }
+    });
+  }
+
+  void _startRecording(TapDownDetails details) {
+    _buttonAnimationController.forward();
+    _progressAnimationController.forward();
+  }
+
+  void _stopRecording() {
+    _buttonAnimationController.reverse();
+    _progressAnimationController.reset();
   }
 
   @override
   Widget build(BuildContext context) {
+    print(_progressAnimationController.value);
     return Scaffold(
       body: !_hasPermission || !_cameraController.value.isInitialized
           ? const DefaultLoading(message: "Requesting permissions")
           : Center(
               child: Stack(
+                alignment: Alignment.center,
                 children: [
                   CameraPreview(_cameraController),
+                  Positioned(
+                    bottom: 48,
+                    child: GestureDetector(
+                      onTapDown: _startRecording,
+                      onTapUp: (_) => _stopRecording(),
+                      onLongPressEnd: (_) => _stopRecording(),
+                      child: ScaleTransition(
+                        scale: _buttonAnimation,
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            SizedBox(
+                              width: 96,
+                              height: 96,
+                              child: CircularProgressIndicator(
+                                color: Colors.redAccent,
+                                strokeWidth: 6,
+                                value: _progressAnimationController.value,
+                              ),
+                            ),
+                            Container(
+                              width: 84,
+                              height: 84,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.redAccent,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                   Positioned(
                     top: 24,
                     right: 24,
